904ea0e8e6263b75867f7d94249d21794a6881bc
Paul_Frenkel <paul.frenkel@uni-rostock.de>
2019-01-15 10:13:28 +0100

    Aufgaben

---
+++ Lab_exercise_2/LabExercise_2_additional_files/sys_help.m
@@ -0,0 +1,5 @@
+function [xosys,xo] = sys(xi,fs)
+%[xosys,xo] = sys2(xi,fs)
+% fs       : sampling frequency
+% xi       : 2-channel input vector (2-column vector)
+% xo/xosys : 2-channel output vectors (2-column vectors)
