e477384c0c6f113c8ea8d665d181561c40ee4613
paul_frenkel <paul.frenkel@uni-rostock.de>
2019-02-10 22:29:48 +0100

    Aufgaben überarbeitet und vervollständigt.

--- Lab_exercise_2/lab_2_1.m
+++ Lab_exercise_2/lab_2_1.m
@@ -1,45 +1,36 @@
 %% a) sine
-f = 1000;
-fs = 48000;
-amp = 1;
-N = 96001;
+fs=48000; % sample frequency
+N=96001; % sample points
+n=0:N-1;
+t=n/fs;
 
-t = 0:1/fs:(N-1)/fs;
-sn = amp * sin(2*pi*f*t);
+f0=1000; % signal frequency
+sn =sin(2*pi*f0*t);
 
-% figure(1);
-% plot(sn);
-% title('sine');
+df = fs/N;
+df_axis = 1 : df : fs/2;
 
 %% b) white noise
 wn = wgn(1, N, 0);
 
-% figure(2);
-% plot(wn, 'r');
-% title('white noise');
-
 %% c) pink noise
-WN = fft(wn);
+WN = fft(wn);   % fft
 
 NumUniquePts  = (N+1)/2 +1;
-k = 1:NumUniquePts;
-PNAmpl = WN./sqrt(N);
+k = 1:NumUniquePts; 
+PNAmpl = WN./sqrt(N);   %normalise
 
 PN = PNAmpl.*WN;
 pn = ifft(PN);
-
-% figure(3);
-% plot(pn,'m');
-% title('pink noise');
 
 %% d) mean
 mean_sn = mean(sn);
 mean_wn = mean(wn);
 mean_pn = mean(pn);
 
-fprintf('\nmean of sine: %d\n', mean_sn);
-fprintf('mean of white noise: %d\n', mean_wn);
-fprintf('mean of pink noise: %d\n', mean_pn);
+fprintf('\nMean of sine: %d\n', mean_sn);
+fprintf('Mean of white noise: %d\n', mean_wn);
+fprintf('Mean of pink noise: %d\n\n', mean_pn);
 
 %% e) rms
 sn_dBrms = 20*log10( rms(sn) );
@@ -52,38 +43,106 @@ sn_dBpeak = 20*log10( max(sn) );
 wn_dBpeak = 20*log10( max(wn) );
 pn_dBpeak = 20*log10( max(pn) );
 
+fprintf('Peak value of sine: %d dBu_peak\n', sn_dBpeak);
+fprintf('Peak value of white noise: %d dBu_peak\n', wn_dBpeak);
+fprintf('Peak value of pink noise: %d dBu_peak\n\n', pn_dBpeak);
+
 %creast factor
 sn_CrestdB = sn_dBpeak - sn_dBrms;
 wn_CrestdB = wn_dBpeak - wn_dBrms;
 pn_CrestdB = pn_dBpeak - pn_dBrms;
 
-%% g) spectrum
+fprintf('Crest factor of sine: %d dB\n', sn_CrestdB);
+fprintf('Crest factor of white noise: %d dB\n', wn_CrestdB);
+fprintf('Crest factor of pink noise: %d dB\n', pn_CrestdB);
+
+%% g) FFT analyser
 %fft
-SN_fft = fft(sn);
-SN_norm = 2.*SN_fft ./N;
-SN_dB = 20*log10(abs(SN_norm));
+
+SN_fft = fft(sn,N); % fft
+SN_fft = abs(SN_fft).*(2/N); % normalise
+SN_fft = 20*log10(SN_fft); % dB
 
 WN_fft = fft(wn);
-WN_norm = 2.*WN_fft ./N;
-WN_dB = 20*log10(WN_norm);
+WN_fft = abs(WN_fft).*(2/N);
+WN_fft = 20*log10(WN_fft);
 
 PN_fft = fft(pn);
-PN_norm = PN_fft .* (2/N);
-PN_dB = 20*log10(PN_norm);
+PN_fft = abs(PN_fft).*(2/N);
+PN_fft = 20*log10(PN_fft);
 
 %plot
 figure(1);
 hold on;
-plot( abs(PN_dB), 'r');
-plot( abs(WN_dB), 'c');
-plot( abs(SN_dB), 'g');
+plot(df_axis, PN_fft(1:length(df_axis)), 'r');
+title('FFT analyser');
+plot(df_axis, WN_fft(1:length(df_axis)), 'color', [0.75, 0.75, 0]);
+plot(df_axis, SN_fft(1:length(df_axis)), 'b');
 set(gca, 'XScale', 'log');
-set(gca,'xtick', 1000*(1:10)) 
-xlim([f, fs/2]);
-xticklabels({'1','2','3','4','5','6','7','8','9','10','20'})
+xlim([2, fs/2+1]);
 xlabel('frequency / kHz')
 ylabel('amplitude / dB')
 legend('pink noise', 'white noise', 'sine');
 
-%% h)
+%% h) Real-time analyser
+% generate frequencies
+f_band = zeros(3,9);
+f_band(1,1) = 44.194; % start lower corner frequency
+f_band(2,1) = 62.5; % start centre frequency
 
+for idx = 1:9
+    f_band(3,idx) = 2*f_band(1,idx); % higher corner frequencies
+    if idx < 9
+        f_band(2,idx+1) = 2*f_band(2,idx); % centre frequencies
+        f_band(1,idx+1) = f_band(3,idx); % lower corner frequencies
+    end
+end
+
+% N_1 / N_2
+N_1 = zeros(1,9);
+N_2 = zeros(1,9);
+
+for idx_freq = 1:length(df_axis)
+    if df_axis(idx_freq) >= f_band(1,1)
+        N_1(1) = idx_freq;
+        break;
+    end
+end
+for idx = 1:9
+    for idx_freq = 1:length(df_axis)
+        if df_axis(idx_freq) <= f_band(3,idx)
+            N_2(idx) = idx_freq;
+        end
+    end
+    if idx < 9
+        N_1(idx+1) = N_2(idx)+1;
+    end
+end
+
+% calculate
+SN_rta = zeros(1, 9);
+WN_rta = zeros(1, 9);
+PN_rta = zeros(1, 9);
+
+for idx = 1:9
+    for n = N_1(idx):N_2(idx)
+        SN_rta(idx) = sqrt(abs((2/n)*SN_fft(n))^2)+SN_rta(idx);
+        WN_rta(idx) = sqrt(abs((2/n)*WN_fft(n))^2)+WN_rta(idx);
+        PN_rta(idx) = sqrt(abs((2/n)*PN_fft(n))^2)+PN_rta(idx);
+    end
+    SN_rta(idx) = 20*log10(SN_rta(idx));
+    WN_rta(idx) = 20*log10(WN_rta(idx));
+    PN_rta(idx) = 20*log10(PN_rta(idx));
+end
+
+figure(2);
+hold on;
+bar(SN_rta, 'b');
+bar(PN_rta, 'r');
+bar(WN_rta, 'FaceColor', [0.75, 0.75, 0]);
+set(gca, 'XTickLabel', {f_band(2,:)})
+xlim([0.5 9.5])
+title('Real-time analyser');
+xlabel('centre frequency / Hz')
+ylabel('peak / dB')
+legend('sine', 'pink noise', 'white noise');
